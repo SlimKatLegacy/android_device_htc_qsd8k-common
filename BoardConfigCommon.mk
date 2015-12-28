e6c9a72f5a6125884f1984004ca0e1c815365f6a
spezi77 <spezi77@gmx.com>
2015-12-28 22:46:57 +0100

    Moved kernel config to bravo device tree.
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -108,13 +108,6 @@ COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 BOARD_USE_OLD_AVC_ENCODER := true
 BOARD_NO_BFRAMES := true
 
-# Kernel directory
-TARGET_KERNEL_SOURCE := kernel/htc/qsd8k
-BUILD_KERNEL := true
-
-# Override kernel toolchain. (4.6 is too unstable)
-KERNEL_TOOLCHAIN_PREFIX:=$(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.4.3/bin/arm-eabi-
-
 # TWRP
 DEVICE_RESOLUTION := 480x800
 # no kernel support
